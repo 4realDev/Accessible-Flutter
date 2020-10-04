43888fdfc6c12e8f7547b709b18d0cddd7a288ec
Vladimir_Brazhnik <v.brazhnik@web.de>
2020-10-04 21:20:56 +0200

    Add optimization to the product_list_item
    "Double tap to add to cart" instead of "Double tap to activate"
    Reading only the product name and the product price.
    Not reading the "Add Button" anymore.
    
    Before: "Product name. Rroduct price. Add Button, double tap to activate."
    After: "Product name. Product price. Double tap to add to cart."

--- accessible-flutter-optimized/lib/layout/product_list_item.dart
+++ accessible-flutter-optimized/lib/layout/product_list_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 
 import '../model/app_state_model.dart';
@@ -28,62 +29,102 @@ class ProductRowItem extends StatelessWidget {
         right: 8,
       ),
 
-      child: Row(
-        children: <Widget>[
+      // Raw Screen-Reading: "Product name. Rroduct price. Add Button, double tap to activate."
+      // Step 1: Wrap whole item with the Semantics Widget
 
-          /*** IMAGE ***/
-          ClipRRect(
-            borderRadius: BorderRadius.circular(4),
-            child: Image.asset(
-              product.assetName,
-              package: product.assetPackage,
-              fit: BoxFit.cover,
-              width: 76,
-              height: 76,
-            ),
-          ),
+      // Step 2: Create own logic for the onTap event and adjust the onTapHint to "Double tap to add to cart."
+      // Problem: Now we have two items, that can be tapped/pressed.
+      // Because of this the CupertinoButton is now seen as a separate Semantic Widget in the Semantic-Tree
+      // Screen-Reading: "Product name. Product price. Double tap to add to cart." SWIPE_LEFT "Add Button. Double tap to activate."
+      // Possible Solution: Wrap everything in a MergeSemantics Widget - Problem: the Semantic Meaning of the CupertinoButton will be still visible and readable for the screen-readers
+      // Screen-Reading: "Product name. Product price. ADD BUTTON. Double tap to add to cart."
+      // Solution: Step 3
 
-          /*** TEXT ***/
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 12),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
+      // Step 3: Exclude the CupertinoButton from the Semantic tree
+      // Exclude the ClipRRect as well, because it has no semantic meaning
+      // To achieve this, wrap the CupertinoButton and the ClipRRect with the ExcludeSemantics Widget
+      // This achieves as well, that the onPressed() event can only be triggered when the screen-reader is disabled,
+      // and on the opposite the onTap() event can not be triggered when the screen-reader is enabled
+      // Screen-Reading: "Product name. Product price. Double tap to add to cart."
 
-                  /*** PRODUCT NAME ***/
-                  Text(
-                    product.name,
-                    style: Styles.productRowItemName,
-                  ),
+      child: Semantics(
+        onTap: () {
+          final model = Provider.of<AppStateModel>(context, listen: false);
+          model.addProductToCart(product.id);
+        },
 
-                  /*** PRODUCT PRICE ***/
-                  const Padding(padding: EdgeInsets.only(top: 8)),
-                  Text(
-                    '\$${product.price}',
-                    style: Styles.productRowItemPrice,
-                  )
+        // onTapHint completes the "Double tap to" sentence with the given string
+        onTapHint: "add to cart",
 
-                ],
+        child: Row(
+          children: <Widget>[
+
+            /*** #region IMAGE ***/
+            // to ensure that screen-readers are not reading "Image"
+            ExcludeSemantics(
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(4),
+                child: Image.asset(
+                  product.assetName,
+                  package: product.assetPackage,
+                  fit: BoxFit.cover,
+                  width: 76,
+                  height: 76,
+                ),
               ),
             ),
-          ),
 
-          /*** ICON ***/
-          CupertinoButton(
-            padding: EdgeInsets.zero,
-            onPressed: () {
-              final model = Provider.of<AppStateModel>(context, listen: false);
-              model.addProductToCart(product.id);
-            },
-            child: const Icon(
-              CupertinoIcons.plus_circled,
-              semanticLabel: 'Add',
+
+            /*** TEXT ***/
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 12),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+
+                    /*** PRODUCT NAME ***/
+                    Text(
+                      product.name,
+                      style: Styles.productRowItemName,
+                    ),
+
+                    const Padding(padding: EdgeInsets.only(top: 8)),
+
+                    /*** PRODUCT PRICE ***/
+                    Text(
+                      '\$${product.price}',
+                        style: Styles.productRowItemPrice,
+                    ),
+
+                  ],
+                ),
+              ),
             ),
-          ),
 
-        ],
+
+            /*** BUTTON ***/
+            ExcludeSemantics(
+              child: CupertinoButton(
+                padding: EdgeInsets.zero,
+                onPressed: () {
+                  final model = Provider.of<AppStateModel>(context, listen: false);
+                  model.addProductToCart(product.id);
+                },
+
+                /*** ICON ***/
+                child: const Icon(
+                  CupertinoIcons.plus_circled,
+                  semanticLabel: 'Add',
+                ),
+
+              ),
+            ),
+
+
+          ],
+        ),
       ),
     );
 
