bdc1544dda0baa5bb502d8d5dc5847f34a048c20
Vladimir_Brazhnik <v.brazhnik@web.de>
2020-11-16 03:12:34 +0100

    Add strings for shopping_cart_item and shopping_cart_tab

--- accessible-flutter-optimized/lib/language_adapted_strings.dart
+++ accessible-flutter-optimized/lib/language_adapted_strings.dart
@@ -2,15 +2,28 @@ import 'dart:io';
 
 abstract class LanguageAdaptedStrings {
 
+  /*** PRODUCT TAB / PRODUCT ITEM ***/
   static String productItemHint;
-  static String clearButtonHint;
-  static String clearButtonLabel;
   static String productTabDescription;
-  static String productCounterSemantic;
   static String productAddAnnouncementSemantic;
+
+  /*** TAB BAR ***/
   static String productTab;
   static String searchTab;
   static String cartTab;
+
+  /*** CART TAB / CART ITEM ***/
+  static String cartItemHint;
+  static String cartItemRemoveAnnouncementSemantic;
+  static String cartListHeading;
+  static String cartListCount;
+  static String cartListHintSemantic;
+
+  static String productCounterSemantic;
+
+  /*** TEXTFIELD ***/
+  static String clearButtonHint;
+  static String clearButtonLabel;
 
   static setLanguageAdaptedString() {
     final String systemLanguage = Platform.localeName; // Returns locale string in the form 'en_US' || 'de_DE';
@@ -18,12 +31,21 @@ abstract class LanguageAdaptedStrings {
     switch(systemLanguage) {
       case 'de_DE': {
         // 'Zum x doppelt klicken' is given as Talkback default for Hints
+        productTabDescription = 'Willkommen im Cupertino Store. Bitte wählen Sie Ihre gewünschten Produkte aus und gehen Sie zum Warenkorb Tab, um diese zu bestellen.';
         productItemHint = 'in den Warenkorb hinzufügen';
+        productAddAnnouncementSemantic = 'zum Warenkorb hinzugefügt.';
+
+        cartItemHint = 'aus dem Warenkorb entfernen.';
+        cartItemRemoveAnnouncementSemantic = 'aus dem Warenkorn entfernt.';
+        cartListHeading = 'Warenkorb Liste:';
+        cartListCount = 'Produkte sind aktuell im Warenkorb.';
+        cartListHintSemantic = 'Wischen Sie nach rechts um die Produkte anzuhören.';
+
+        productCounterSemantic = 'Mal zum Warenkorb hinzugefügt.';
+
         clearButtonHint = 'Löschen des Textes';
         clearButtonLabel = 'Lösch Button';
-        productTabDescription = 'Willkommen im Cupertino Store. Bitte wählen Sie Ihre gewünschten Produkte aus und gehen Sie zum Warenkorb Tab, um diese zu bestellen.';
-        productCounterSemantic = 'Mal zum Warenkorb hinzugefügt.';
-        productAddAnnouncementSemantic = 'zum Warenkorb hinzugefügt.';
+
         productTab = 'Produkte';
         searchTab = 'Suche';
         cartTab = 'Warenkorb';
@@ -32,12 +54,21 @@ abstract class LanguageAdaptedStrings {
 
       // 'Double tap to x' is given as Talkback default for Hints
       default: {  // default represents the language English with 'en_Us'
+        productTabDescription = 'Welcome to the Cupertino Store. Please select your desired products and go to the Cart Tab to order them.';
         productItemHint = 'add to cart';
+        productAddAnnouncementSemantic = 'added to cart.';
+
+        cartItemHint = 'remove from cart.';
+        cartItemRemoveAnnouncementSemantic = 'removed from cart.';
+        cartListHeading = 'Shopping Cart List:';
+        cartListCount = 'products currently added.';
+        cartListHintSemantic = 'Swipe right to hear the products.';
+
+        productCounterSemantic = 'times added to cart.';
+
         clearButtonHint = 'clear the text';
         clearButtonLabel = 'Clear Button';
-        productTabDescription = 'Welcome to the Cupertino Store. Please select your desired products and go to the Cart Tab to order them.';
-        productCounterSemantic = 'times added to cart.';
-        productAddAnnouncementSemantic = 'added to cart.';
+
         productTab = 'Products';
         searchTab = 'Search';
         cartTab = 'Cart';
